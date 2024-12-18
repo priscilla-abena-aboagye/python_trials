c22f1d958e21c5cce755c5656e99274a9a6219c1
Abena <priscillaabenaaboagye@gmail.com>
2024-12-18 16:39:06 +0000

    Update test.py

--- test.py
+++ test.py
@@ -162,6 +162,25 @@ while True:
 interest = principle * (1 + (rate / 100)) ** time
 print(f"Balance after {time} year/s: ${interest:,.2f}")
 
+for x in range(1,21):
+    if x == 13:
+        continue
+    else:
+        print(x)
+
+import time
+my_time = int(input("Enter the time in seconds: "))
+
+for x in range(my_time, 0, -1):
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+
+    time.sleep(1)
+
+
+print("TIME'S UP")
 
 
 
