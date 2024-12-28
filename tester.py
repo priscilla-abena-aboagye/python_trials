1cdf7a60f42ea95f6f5010f6b3e61518ced1b7a0
Abena <priscillaabenaaboagye@gmail.com>
2024-12-28 11:27:03 +0000

    Dictionary

--- tester.py
+++ tester.py
@@ -68,3 +68,37 @@ capitals.popitem()
 print(capitals)
 
 
+
+
+
+menu = {"popcorn": 4.00,
+        "chips": 3.00,
+        "fries": 6.00,
+        "burger": 12.00,
+        "soda": 10.00,
+        "lemonade": 4.25}
+
+cart = []
+total = 0
+print("------------MENU------------")
+for key, value in menu.items():
+    print(f"{key:10}: ${value:.2f}")
+print("------------------------")
+
+while True:
+    food = input("Select an item to buy (press q to quit) :").lower()
+    if food == "q":
+        break
+    elif menu.get(food) is not None:
+        cart.append(food)
+
+for food in cart:
+    total += menu.get(food)
+    print(food, end=" ")
+
+print()
+print(f"The total is {total:.2f}")
+
+
+
+
