3057cfbee48e8e6863338299fd02d7742543d274
Abena <priscillaabenaaboagye@gmail.com>
2024-12-30 11:08:40 +0000

    Game

--- tester.py
+++ tester.py
@@ -134,3 +134,38 @@ while is_running:
         print("Invalid guess")
         print(f"Please select a number between {lowest_num} and {highest_num}")
 
+
+
+
+options = ("rock", "paper", "scissors")
+
+running = True
+
+while running:
+
+    player = None
+    computer = random.choice(options)
+    while player not in options:
+        player = input("Enter a choice (rock, paper, scissors): ")
+
+    print(f"Player : {player}")
+    print(f"Computer: {computer}")
+
+    if player == computer:
+        print("It's a tie")
+    elif player == "rock" and computer == "scissors":
+        print("You win!")
+    elif player == "paper" and computer == "rock":
+        print("You win!")
+    elif player == "scissors" and computer == "paper":
+        print("You win!")
+    else:
+        print("You lose!")
+
+    play_again = input("Play again? (y/n): ").lower()
+    if not play_again == "y":
+        running = False
+
+print("Thanks for playing")
+
+
